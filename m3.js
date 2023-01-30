c8ba28655d39416a6f81af4316339db3fda4442d
METR0109 <110671168+METR0109@users.noreply.github.com>
2023-01-30 21:53:17 +0200

    aa

--- m3.js
+++ m3.js
@@ -141,17 +141,101 @@
 // console.log(product);
 
 // ---9---
-const emailInputName = 'email';
-const passwordInputName = 'password';
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
 
-const credentials = {
-  // Change code below this line
-  // Change code above this line
-};
+// const credentials = {
+//   // Change code below this line
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//   [passwordInputName]: 'jqueryismyjam',
+//   // Change code above this line
+// };
+// console.log(credentials);
 
 // ---10---
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості з таким ключем
+//   console.log(book[key]);
+// }
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = ['descr', 'rating', 'price'];
+// const values = ['Spacious apartment in the city center', 4, 2153];
+// // for (const keys in apartment) {
+// //   console.log(keys);
+// //   console.log(apartment[keys]);
+// // }
+// // for (const values in apartment) {
+// //   console.log(values);
+// //   console.log(apartment[values]);
+// // }
+// for (const keys in apartment) {
+// }
+// for (const values in apartment) {
+// }
+// Change code below this line
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment) {
+//   // Ключ
+//   keys.push(key);
+//   console.log(keys);
+//   // Значення властивості з таким ключем
+//   values.push(apartment[key]);
+//   console.log(values);
+// }
+
 // ---11---
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Spacious apartment in the city center';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+
+//     // Change code above this line
+//   }
+// }
+
 // ---12---
+function countProps(object) {
+  // Change code below this line
+  let propCount = 0;
+  const keys = Object.keys(object);
+  for (let key of keys) {
+    propCount += 1;
+  }
+  return propCount;
+  // Change code above this line
+}
+
 // ---13---
 // ---14---
 // ---15---
