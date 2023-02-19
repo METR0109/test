1293f3929f0c220b7222e2df3f22281be005bea6
METR0109 <110671168+METR0109@users.noreply.github.com>
2023-02-19 09:32:35 +0200

    aaa

--- m3.js
+++ m3.js
@@ -278,9 +278,59 @@
 // }
 
 // ---15---
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
 // ---16---
+
+// function countTotalSalary(obj) {
+//   let totalSalary = 0;
+//   for (const salary of Object.values(obj)) {
+//     totalSalary += salary;
+//   }
+//   return totalSalary;
+// }
+
 // ---17---
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
 // ---18---
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.price;
+    }
+  }
+  return null;
+}
+
 // ---19---
 // ---20---
 // ---21---
